d2ab128df177565262565870cdc08eda13f8d194
Lin <412570144@m365.fju.edu.tw>
2024-11-28 14:28:49 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const app = express();
+const PORT = 3000;
+
+// 中介軟體設置
+app.use(bodyParser.json());
+app.use(cors());
+
+// 路由
+const transactionRoutes = require('./routes/api');
+app.use('/api', transactionRoutes);
+
+// 主頁面靜態資源
+app.use(express.static('public'));
+
+// 啟動伺服器
+app.listen(PORT, () => {
+    console.log(伺服器正在 http://localhost:${PORT} 運行);
+});
