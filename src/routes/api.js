d56b2260b8cd719389536d4e577fe80771e44e0b
Lin <412570144@m365.fju.edu.tw>
2024-11-28 14:29:38 +0800

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { getAllTransactions, addTransaction, deleteTransaction } = require('../controllers/transaction');
 
+// 取得所有交易
+router.get('/transactions', getAllTransactions);
+
+// 新增交易
+router.post('/transactions', addTransaction);
+
+// 刪除交易
+router.delete('/transactions/:id', deleteTransaction);
+
+module.exports = router;
